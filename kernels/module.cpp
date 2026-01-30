a3cf6f5176d72686dc976b42b67aa317e6fc1b45
praha6248 <natalia.plocha6248@gmail.com>
2026-01-30 23:37:17 +0100

    Revert "adding adaptive threshold"
    This reverts commit 04cb029bea55e9a0ec19635a9f36bdefca542fad.

--- kernels/module.cpp
+++ kernels/module.cpp
@@ -6,7 +6,6 @@ torch::Tensor box_blur(torch::Tensor img, int blurSize);
 torch::Tensor sobel(torch::Tensor img);
 torch::Tensor dilation(torch::Tensor img, int filterSize);
 torch::Tensor erosion(torch::Tensor img, int filterSize);
-torch::Tensor adaptive_threshold_tensor(torch::Tensor input, double max_value, int method, int type, int blockSize, double C);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
     m.def("rgb2gray", &rgb_to_gray, "rgb to grayscale kernel");
@@ -14,5 +13,4 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
     m.def("sobel", &sobel, "sobel filter kernel");
     m.def("dilate", &dilation, "dilation kernel");
     m.def("erode", &erosion, "erosion kernel");
-    m.def("adaptive_threshold", &adaptive_threshold_tensor, "adaptive threshold kernel");
 }
